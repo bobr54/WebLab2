d9daa87f4dbefe9308489711bf78f52a46ab22fc
Viktor Gritskov <viktor.g010803@gmail.com>
2023-09-18 23:51:36 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Add some change for git");
+        System.out.println("Add some change for git1");
     }
 }
